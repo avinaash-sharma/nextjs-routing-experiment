f8e83995faa750c2617bf524adeee348e5b19fb9
Avinash Sharma <avi.rohit13@hotmail.com>
2024-09-07 22:35:33 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Customers = () => {
+    return (
+        <div className="flex flex-col">
+            <h1 className="mb-4 text-3xl font-bold">Customers</h1>
+        </div>
+    );
+};
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Invoices = () => {
+    return (
+        <div className="flex h-full flex-col">
+            <h1 className="mb-4 text-2xl font-bold">Invoices</h1>
+        </div>
+    );
+};
+
+export default Invoices
